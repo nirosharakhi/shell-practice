d886ce40abf35f85185e20d496f523d9a2e4b846
nirosha <nirosharakhi250519@gmail.com>
2025-10-04 17:14:29 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Trump::Hey musk, how are you?"
+echo "musk::hello trump, iam fine. how are you doing?"
+echo "Trump:iam fine too.whatsup?"
+echo "musk::Nothing,Just going to mars now, will you come?"
+echo "Trump::sorry,you carry on! i will come once you come back"
