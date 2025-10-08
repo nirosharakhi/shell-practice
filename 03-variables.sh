2e046ba0ae38beb4ab7b450d41dc8cbabf48f89f
nirosha <nirosharakhi250519@gmail.com>
2025-10-08 11:43:46 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1 = Trump
+PERSON2 = Musk
+echo "$PERSON1::Hey $PERSON2,How are you?"
+echo "$PERSON2::Hello $PERSON1,Iam fine. How are you doing?"
+echo "$PERSON1:Iam fine too.what's up?"
+echo "$PERSON2::Nothing, just going to mars now, will you come?"
+echo "$PERSON1::sorry, you carry on i will come once you come back????"
