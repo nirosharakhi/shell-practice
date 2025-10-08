1f2c5823dd40a030335e4cc37d3b0891d1dbd0b1
nirosha <nirosharakhi250519@gmail.com>
2025-10-08 13:06:05 +0530

    variables

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -4,4 +4,4 @@ NUMBER2=200
 TIMESTAMP=$(date)
 echo "script executed at : $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
-echo "sum of $NUMBER1 and $NUMBER2 is :$SUM
+echo "sum of $NUMBER1 and $NUMBER2 is :$SUM"
