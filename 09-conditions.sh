4db5e64741cf748c393b61783c0f14788620feaf
nirosha <nirosharakhi250519@gmail.com>
2025-10-31 12:53:50 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,5 +4,5 @@ if [ $NUMBER -lt 10]
 then
     echo "Given number $NUMBER  is less than 10"
 else 
-    echo "Given number $NUMBER is less than 10"
+    echo "Given number $NUMBER is not less than 10"
 fi
