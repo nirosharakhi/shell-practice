93410b3d0e930239d1deffff3f0ad25877054d18
nirosha <nirosharakhi250519@gmail.com>
2025-11-01 16:12:20 +0530

    install commands

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -6,5 +6,21 @@ then
     exit 1
     else
     echo "you are running with root access"
-fi
+fi 
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "MYSQl is not installed.... going to install it"
+    dnf install mysql
+    if [ $? -ne 0 ]
+    then 
+        echo "installing mysql....success"
+    else 
+        echo "installing mysql... failure"
+    exit 1
+    fi
+else 
+    echo "Mysql is already installed..nothing to do"
+fi
