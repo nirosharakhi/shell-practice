6de09ce26ef22e43cb30381a1a579688e2feaa8a
nirosha <nirosharakhi250519@gmail.com>
2025-11-01 16:43:02 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
-USERID = $(id -u)
-if [$USERID -ne 0]
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
 then
-    echo "Error:Please run this script with root access"
-    exit 1
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #give other than 0 upto 127
 else
-    echo "you are running with root access"
+    echo "You are running with root access"
 fi
 
-VALIDATE()
-{
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
     if [ $1 -eq 0 ]
-    then  
-        echo "installing $2 is success"
+    then
+        echo "Installing $2 is ... SUCCESS"
     else
-        echo "installing $2 is failure"
+        echo "Installing $2 is ... FAILURE"
         exit 1
     fi
 }
 
-dnf list mysql 
+dnf list installed mysql
 if [ $? -ne 0 ]
-then 
-    echo "mysql is not installed going to install it"
-    dnf list mysql
-    VALIDATE $? "mysql"
+then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
 else
-    echo "Mysql is already installed...nothing to do"
+    echo "MySQL is already installed...Nothing to do"
 fi
 
-dnf list python3
+dnf list installed python3
 if [ $? -ne 0 ]
-then 
-    echo "python is not installed going to install it"
-    dnf list python
+then
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
     VALIDATE $? "python3"
 else
-    echo "python3 is already installed...nothing to do"
+    echo "python3 is already installed...Nothing to do"
 fi
 
-dnf list nginx
+dnf list installed nginx
 if [ $? -ne 0 ]
-then 
-    echo "nginx is not installed going to install it"
-    dnf list nginx
+then
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
     VALIDATE $? "nginx"
 else
-    echo "nginx is already installed...nothing to do"
+    echo "nginx is already installed...Nothing to do"
 fi
-
